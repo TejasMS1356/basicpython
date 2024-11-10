bea46026cfffabe0337dabd7365860e14fa73d51
TEJAS MS <tejastejasms71@gmail.com>
2024-11-10 09:27:29 +0530

    Add files via upload

---
+++ encrypt.py
@@ -0,0 +1,28 @@
+import random
+import string
+char=string.digits+string.ascii_letters+string.punctuation+" "
+char=list(char)
+
+key=char.copy()
+random.shuffle(key)
+
+message=input("enter the message: ")
+cipher_text=""
+for letter in message:
+   # random.shuffle(key)
+    index=char.index(letter)
+    cipher_text+=key[index]
+print(f"entered message is:{message}")
+print(f"encrypted message is:{cipher_text}")
+
+
+#decryption
+chiper_text=input("enter encrypted message: ")
+message=""
+for letter in chiper_text:
+    
+    index=key.index(letter)
+    message+=char[index]
+print(f"encrypted message is {cipher_text}")
+print(f"decryped message is:{message}")
+
