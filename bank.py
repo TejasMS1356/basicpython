a02ffcd93540d6d79567e830c7c9ea1012ce0289
TEJAS MS <tejastejasms71@gmail.com>
2024-11-12 20:34:22 +0530

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,55 @@
+#DICTIONARY OF ACCOUNT NO AND AMOUNT
+d = {
+        12345678: 45000,
+        23456789: 36789
+    }
+#ANOTHER DICTIONARY OF ACC NO AND IFSC CODE
+ifsccode={
+       12345678 :"WERTY6373",
+       23456789 :"asdfg3454"
+    }
+#FUNCTION TO CHECKA AND PRINT THE AMOUNT
+def bank(ac, ifsc):
+    if ac in d and ifsc == ifsccode.get(ac):
+        print("THE AMOUNT PRESENT IN ACC IS:",d[ac])
+       
+        return True,d
+    else:
+        print("Not matching")
+        print("Try again! You have 3 attempts")
+        return False,d
+print("**** WELCOME TO AMOUNT CHECKING SOFTWARE*****")
+i = 0
+#WHILE LOOP FOR ENTERING THE CORRECT ACC NO
+while i<3:
+    
+    ac = int(input("Enter YOUR CORRECT ACCOUNTNUMBER: "))
+    ifsc = input("Enter the IFSC code(IN CAPITALS): ").upper()
+    r,d=bank(ac, ifsc)
+    
+    if r:
+        #WHILE LOOP FOR CONTINIOUS TRANSACTIONS
+        while True:
+            s=input("ENTER THE CHOICE CREDIT/DEBIT(1/2):")
+            if s=="1":
+                c=int(input("ENTER THE AMOUNT TO CREDIT  IN YOUR BANK ACCOUNT: "))
+                d[ac]+=c
+                print("NEW BALANCE  IS ",d[ac])
+            elif s=="2":
+                c=int(input("ENTER THE AMOUNT TO BE WITHDRAWAL: "))
+                d[ac]-=c
+                print("CURRENT BALANCE: ",d[ac])
+            k=int(input("ENTER 3  TO CONTINUE THE TRANSACTION OR 4 TO EXIT: "))
+            if k!=3:
+                print("****THANK YOU! VISIT AGAIN*****")
+                break
+        break
+    i+=1
+
+
+        
+        
+    
+   
+        
+
